b1b34553fd13a84d1b675ca7099d124c3dae885e
MichelleTirado <mich.stirado@gmail.com>
2021-09-24 13:41:13 +0200

    First Upload Project

---
+++ app/src/main/java/com/example/u1_practica1_layoutsymaslayouts/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.u1_practica1_layoutsymaslayouts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
